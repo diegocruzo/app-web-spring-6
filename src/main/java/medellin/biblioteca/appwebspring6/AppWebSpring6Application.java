8f016a93a3d8a185fc8c537df301db08f21a86c4
Diego Cruz <diegocruzo@gmail.com>
2024-09-07 09:16:37 -0500

    Primer actualización de repositorio remoto

---
+++ src/main/java/medellin/biblioteca/appwebspring6/AppWebSpring6Application.java
@@ -0,0 +1,13 @@
+package medellin.biblioteca.appwebspring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebSpring6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebSpring6Application.class, args);
+	}
+
+}
---
+++ src/test/java/medellin/biblioteca/appwebspring6/AppWebSpring6ApplicationTests.java
@@ -0,0 +1,13 @@
+package medellin.biblioteca.appwebspring6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebSpring6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
